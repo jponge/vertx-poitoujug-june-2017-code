972a90200e980e08bed0abf3547e703117392441
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-06-14 18:54:40 +0200

    Yo

--- src/main/java/livecoding/MainVerticle.java
+++ src/main/java/livecoding/MainVerticle.java
@@ -6,6 +6,12 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
-
+    vertx.createHttpServer()
+      .requestHandler(request -> {
+        request.response()
+          .putHeader("Content-Type", "text/plain")
+          .end("Hello le PoitouJUG");
+      })
+      .listen(8080);
   }
 }
